a1c5e3c80dd5891f5550bf6067959fd37c2e6c47
Andrew Nelson <andy@andyhub.com>
2016-06-17 18:28:06 -0400

    revert 5d731ef09d616ed3ac479ad1ceab05c99dcabf78

--- www/js/controllers/AddCtrl.js
+++ www/js/controllers/AddCtrl.js
@@ -85,14 +85,14 @@ angular.module('controllers')
 
     $scope.blurWhere = function(event) {
         window.addEventListener('native.keyboardhide', function(){
-            $scope.keyboardSpace = "0";
+            window.scrollTo(0, 0);
         });
     };
 
     $scope.focusWhere = function(event) {
+        var rect = event.target.getBoundingClientRect();
         window.addEventListener('native.keyboardshow', function(){
-            var rect = event.target.getBoundingClientRect();
-            $scope.keyboardSpace = 50-1*rect.top+"px";
+            window.scrollTo(0, rect.top);
         });
     };
 
