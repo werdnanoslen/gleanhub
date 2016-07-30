af78532b3420d1dc275062caf17977d6dd8451b4
Andrew Nelson <andy@andyhub.com>
2016-07-29 22:27:21 -0500

    used better radio button for bools

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,1 +1,27 @@
-angular.module('directives', [])
+angular.module('directives', ['ionic'])
+
+.directive('groupedRadio', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        scope: {
+            model: '=ngModel',
+            value: '=groupedRadio'
+        },
+        link: function(scope, element, attrs, ngModelCtrl) {
+            element.addClass('button button-small');
+            element.on('click', function(e) {
+                scope.$apply(function() {
+                    ngModelCtrl.$setViewValue(scope.value);
+                });
+            });
+
+            scope.$watch('model', function(newVal) {
+                element.removeClass('button-positive');
+                if (newVal === scope.value) {
+                    element.addClass('button-positive');
+                }
+            });
+        }
+    };
+})
