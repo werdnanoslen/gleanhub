8d86a7ea6e13c166155547f2d7a60204af8eccd4
Andrew Nelson <andy@andyhub.com>
2016-08-13 16:29:41 -0500

    improved mysql connection pooling, simplified server code, hopefully won't crash all the time now...

--- api/server.js
+++ api/server.js
@@ -1,14 +1,32 @@
-var express = require("express");
-var mysql   = require("mysql");
-var bodyParser  = require("body-parser");
-var rest = require("./rest.js");
-var app  = express();
+var http = require('http');
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var cloudinary = require('cloudinary');
 var secrets = require("./secrets.js");
 
-function REST(){
-    var self = this;
-    self.connectMysql();
-};
+// Configure database connection
+function Connection() {
+    this.pool = null;
+
+    this.init = function() {
+        this.pool = mysql.createPool({
+            connectionLimit: 10,
+            host     : secrets.HOSTNAME,
+            user     : secrets.USERNAME,
+            password : secrets.PASSWORD,
+            database : secrets.DATABASE,
+            debug    : true
+        });
+    };
+
+    this.acquire = function(callback) {
+        this.pool.getConnection(function(err, connection) {
+            callback(err, connection);
+        });
+    };
+}
 
 // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
 function allowCrossDomain(req, res, next) {
@@ -25,45 +43,338 @@ function allowCrossDomain(req, res, next) {
     }
 };
 
-REST.prototype.connectMysql = function() {
-    var self = this;
-    var pool      =    mysql.createPool({
-        connectionLimit : 100,
-        host     : secrets.HOSTNAME,
-        user     : secrets.USERNAME,
-        password : secrets.PASSWORD,
-        database : secrets.DATABASE,
-        debug    : false
+// Routes
+var api = secrets.APIPATH;
+var wuApi = secrets.WU_API;
+var columns = ["datetime_occurred", "number", "text", "place", "lat", "lng"];
+cloudinary.config({
+    cloud_name: secrets.CLOUD_NAME,
+    api_key: secrets.CLOUD_KEY,
+    api_secret: secrets.CLOUD_SECRET
+});
+
+// Get API info
+router.get("/", function(req, res) {
+    res.json({
+        "Message": "This is the Gleanhub API"
     });
-    pool.getConnection(function(err,connection){
-        if(err) {
-          self.stop(err);
+});
+
+// Get all reports
+router.get("/reports", function(req, res) {
+    var query = "SELECT * FROM ??";
+    var table = ["reports"];
+    query = mysql.format(query, table);
+    connection.acquire(function(err, con) {
+        con.query(query, function(err, rows) {
+            con.release();
+            if (err) {
+                res.status(500).json({
+                    "error": err
+                });
+            } else {
+                res.json({
+                    "reports": rows
+                });
+            }
+        });
+    });
+});
+
+// Get report by id
+router.get("/reports/:id", function(req, res) {
+    var query = "SELECT * FROM ?? WHERE ??=?";
+    var table = ["reports", "id", req.params.id];
+    query = mysql.format(query, table);
+    connection.acquire(function(err, con) {
+        con.query(query, function(err, rows) {
+            con.release();
+            if (err) {
+                res.status(500).json({
+                    "error": err
+                });
+            } else {
+                res.json({
+                    "report": rows
+                });
+            }
+        });
+    });
+});
+
+// Get reports nearby
+router.post("/reports/nearby", function(req, res) {
+    var query = "SELECT *, ROUND(SQRT(POW(((69.1/1.61) * (? - ??)), 2) + POW(((53/1.61) * (? - ??)), 2)), 1) " +
+        "AS distance FROM ?? HAVING distance < ? ORDER BY distance;";
+    var table = [
+        req.body.myLat.toString(), "lat",
+        req.body.myLng.toString(), "lng",
+        "reports", req.body.kmAway.toString()
+    ];
+    query = mysql.format(query, table);
+    connection.acquire(function(err, con) {
+        con.query(query, function(err, rows) {
+            con.release();
+            if (err) {
+                res.status(500).json({
+                    "error": err
+                });
+            } else if (rows.length < 1) {
+                res.sendStatus(204);
+            } else {
+                res.json({
+                    "reports": rows
+                });
+            }
+        });
+    });
+});
+
+// Get report by filter criteria
+router.post("/reports/filter", function(req, res) {
+    var query = "SELECT * FROM ??";
+    var table = [
+        "reports"
+    ];
+    var keys = Object.keys(req.body);
+    for (var i = 0; i < keys.length; ++i) {
+        query += i ? " AND" : " WHERE";
+        key = keys[i];
+        value = req.body[key];
+        if ("*" === key) {
+            for (var j = 0; j < columns.length; ++j) {
+                query += j ? " OR" : " (";
+                query += " ?? LIKE ?";
+                valueLike = "%" + value + "%";
+                table.push(columns[j], valueLike);
+            }
+            query += " ) ";
         } else {
-          self.configureExpress(connection);
+            query += " ?? LIKE ?";
+            valueLike = "%" + value + "%";
+            table.push(key, valueLike);
         }
+    }
+    query = mysql.format(query, table);
+    connection.acquire(function(err, con) {
+        con.query(query, function(err, rows) {
+            con.release();
+            if (err) {
+                res.status(500).json({
+                    "error": err
+                });
+            } else if (rows.length < 1) {
+                res.sendStatus(204);
+            } else {
+                res.json({
+                    "reports": rows
+                });
+            }
+        });
     });
-}
+});
 
-REST.prototype.configureExpress = function(connection) {
-    var self = this;
-    app.use(bodyParser.urlencoded({limit: '10mb', extended: true }));
-    app.use(bodyParser.json({limit: '10mb'}));
-    var router = express.Router();
-    app.use(allowCrossDomain);
-    app.use(secrets.APIPATH, router);
-    var rest_router = new rest(router,connection);
-    self.startServer();
-}
+// Add report
+router.post("/reports", function(req, res) {
+    var report = req.body.reportJson;
 
-REST.prototype.startServer = function() {
-    app.listen(secrets.PORT, function(){
-      console.log("Gleanhub API running at port " + secrets.PORT);
+    if (report.photo === undefined) {
+        postReport();
+    } else if (report.photo.length > 10) {
+        cloudinary.uploader.upload(report.photo, function(result) {
+            postReport(result.url);
+        });
+    } else {
+        console.error('this doesn\'t look like a base64 image', report.photo);
+        postReport();
+    }
+
+    function postReport(link) {
+        var table = [];
+        var query = "";
+        if (link) {
+            table = [
+                "reports", "availability", "notes", "place", "lat", "lng",
+                "smell", "contents", "cleanFood", "photo",
+                report.availability, report.notes, report.place, report.lat, report.lng,
+                report.smell, report.contents, report.cleanFood, link
+            ];
+            query = "INSERT INTO ??(??,??,??,??,??,??,??,??,??) VALUES (?,?,?,?,?,?,?,?,?)";
+        } else {
+            table = [
+                "reports", "availability", "notes", "place", "lat", "lng",
+                "smell", "contents", "cleanFood",
+                report.availability, report.notes, report.place, report.lat, report.lng,
+                report.smell, report.contents, report.cleanFood
+            ];
+            query = "INSERT INTO ??(??,??,??,??,??,??,??,??) VALUES (?,?,?,?,?,?,?,?)";
+        }
+        query = mysql.format(query, table);
+        connection.acquire(function(err, con) {
+            con.query(query, function(err, rows) {
+                con.release();
+                if (err) {
+                    res.status(500).json({
+                        "error": err
+                    });
+                } else {
+                    res.json({
+                        "report": rows
+                    });
+                }
+            });
+        });
+    }
+});
+
+// Update report
+router.put("/reports/:id", function(req, res) {
+    var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+    var report = req.body.reportJson;
+    var table = ["reports",
+        "active", report.active,
+        "id", req.params.id
+    ];
+    query = mysql.format(query, table);
+    connection.acquire(function(err, con) {
+        con.query(query, function(err, rows) {
+            con.release();
+            if (err) {
+                res.status(500).json({
+                    "error": err
+                });
+            } else {
+                res.json({
+                    "report": rows
+                });
+            }
+        });
     });
-}
+});
 
-REST.prototype.stop = function(err) {
-    console.log("ISSUE WITH MYSQL n" + err);
-    process.exit(1);
-}
+// Delete report by id
+router.delete("/reports/:id", function(req, res) {
+    var query = "DELETE from ?? WHERE ??=?";
+    var table = ["reports", "id", req.params.id];
+    query = mysql.format(query, table);
+    connection.acquire(function(err, con) {
+        con.query(query, function(err, rows) {
+            con.release();
+            if (err) {
+                res.status(500).json({
+                    "error": err
+                });
+            } else {
+                res.json({
+                    "report": rows
+                });
+            }
+        });
+    });
+});
 
-new REST();
+// Get temperature history -48 hours
+router.post("/temp48", function(req, res) {
+    var lat = req.body.lat;
+    var lng = req.body.lng;
+    var url = "http://api.wunderground.com/api/" + wuApi + "/history/q/" +
+        lat + "," + lng + ".json";
+    http.get(url, function(res0) {
+        var body = '';
+        res0.on('data', function(chunk) {
+            body += chunk;
+        });
+        res0.on('end', function() {
+            var fbResponse = JSON.parse(body);
+            var hours = fbResponse.history.observations;
+            var tempData = [];
+            for (var i = 0; i < hours.length; ++i) {
+                tempData.push(hours[i].tempi);
+            }
+            var date = fbResponse.history.date;
+            var urlDate = date.year + date.mon + (Number(date.mday) - 1);
+            var latestHour = hours.length - 1;
+            var url = "http://api.wunderground.com/api/" + wuApi +
+                "/history_" + urlDate + "/q/" + lat + "," + lng + ".json";
+
+            http.get(url, function(res1) {
+                var body = '';
+                res1.on('data', function(chunk) {
+                    body += chunk;
+                });
+                res1.on('end', function() {
+                    var fbResponse = JSON.parse(body);
+                    var hours = fbResponse.history.observations;
+                    for (var i = hours.length - 1; i >= 0; --i) {
+                        tempData.unshift(hours[i].tempi);
+                    }
+                    var date = fbResponse.history.date;
+                    var urlDate = date.year + date.mon + (Number(date.mday) - 1);
+                    var url = "http://api.wunderground.com/api/" + wuApi +
+                        "/history_" + urlDate + "/q/" + lat + "," + lng + ".json";
+
+                    http.get(url, function(res2) {
+                        var body = '';
+                        res2.on('data', function(chunk) {
+                            body += chunk;
+                        });
+                        res2.on('end', function() {
+                            var fbResponse = JSON.parse(body);
+                            var hours = fbResponse.history.observations;
+                            for (var i = hours.length - 1; i > latestHour; --i) {
+                                tempData.unshift(hours[i].tempi);
+                            }
+                            res.json({
+                                "tempData": {
+                                    time: latestHour,
+                                    temps: tempData
+                                }
+                            });
+                        });
+                    }).on('error', function(err) {
+                        res.json({
+                            "tempData": {
+                                time: latestHour,
+                                temps: tempData
+                            }
+                        });
+                        res2.status(500).json({
+                            "error": err
+                        });
+                    });
+                });
+            }).on('error', function(err) {
+                res.json({
+                    "tempData": {
+                        time: latestHour,
+                        temps: tempData
+                    }
+                });
+                res1.status(500).json({
+                    "error": err
+                });
+            });
+        });
+    }).on('error', function(err) {
+        res.status(500).json({
+            "error": err
+        });
+    });
+});
+
+// Start server
+var connection = new Connection();
+var app = express();
+app.use(bodyParser.urlencoded({
+    limit: '10mb',
+    extended: true
+}));
+app.use(bodyParser.json({
+    limit: '10mb'
+}));
+app.use(allowCrossDomain);
+app.use(secrets.APIPATH, router);
+connection.init();
+var server = app.listen(secrets.PORT, function() {
+    console.log('Gleanhub API listening at port ' + server.address().port);
+});
